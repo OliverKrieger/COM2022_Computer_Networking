d9d438e86f4944a075d6c5107e1c0d2445ce1445
OliverKrieger <oliver.krieger76@gmail.com>
2022-03-28 20:44:04 +0100

    request list from another server

--- client.py
+++ client.py
@@ -1,15 +1,17 @@
 import socket
 import sys
 
-print ('Argument List:', str(sys.argv))
-
 def client_init():
     if(sys.argv[1]):
-        Port = int(sys.argv[1])
+        ConnectionPort = int(sys.argv[1])
     else:
-        Port = int(input("Enter Connection Port: "))
+        ConnectionPort = int(input("Enter Connection Port: "))
+
     Dest = "127.0.0.1"
-    ConnectionPort = (Dest, Port)
+    ConnectionPort = (Dest, ConnectionPort)
+
+    print("Client: ", ConnectionPort)
+
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     
     while(True):
--- server.py
+++ server.py
@@ -1,19 +1,37 @@
 import socket
 import sys
+from os import listdir
 
-def server_init():
+def server_init():  
+    if(sys.argv[1]):
+        ConnectionPort = int(sys.argv[1])
+    else:
+        ConnectionPort = int(input("Enter Connection Port: "))
+
     if(sys.argv[2]):
         BindPort = int(sys.argv[2])
     else:
         BindPort = int(input("Enter Bind Port: "))
-        
+
     localIP = "127.0.0.1"
+    ConnectionPort = (localIP, ConnectionPort)
     bufferSize  = 256
+    resourcesPath = "./resources"
+
+    print("Server: ",ConnectionPort, " ", BindPort)
+
     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     UDPServerSocket.bind((localIP, BindPort))
 
     while(True):
         RecvMsg = UDPServerSocket.recvfrom(bufferSize)
         message = RecvMsg[0].decode()
-        address = RecvMsg[1][1]
-        print("{}".format(address), ": {}".format(message))
+        address = RecvMsg[1]
+        port = address[1]
+        if(message == "givelist?"):
+            list = listdir(resourcesPath)
+            list = str.encode("\n" + '\n'.join(list))
+            print(address)
+            UDPServerSocket.sendto(list, ConnectionPort)
+        else:
+            print("{}".format(port), ": {}".format(message))
